a9865a3bdb04f97f87a6334c43a3fc86f2076271
Marcin Filipek <filipekmarcin180@gmail.com>
2022-09-21 11:20:44 +0200

    styled post page, test image

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import styles from "./Post.module.scss";
+import art from "./images/2.png";
 
 export const Post = () => (
   <div className={styles.container}>
-    <p>Post</p>
+    <Image src={art} alt="Post image" width={782} />
+    <div className={styles.content}>
+      <span className={styles.title}>Post title.</span>
+      <p className={styles.date}>10-11-2022</p>
+      <p className={styles.paragraph}>Post content</p>
+    </div>
   </div>
 );
