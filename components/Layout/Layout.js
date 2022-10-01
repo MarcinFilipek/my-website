e0541512ebd11849af6bc6ea7952fd31b5247a26
MarcinFilipek <filipekmarcin180@gmail.com>
2022-10-01 16:19:37 +0200

    Added menu with navigation (#3)

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import styles from "./Layout.module.scss";
+import { Navbar } from "../Navbar";
 
 export const Layout = ({ children }) => (
   <>
     <div className={styles.fixedBackground} />
     <div className={styles.container}>
+      <Navbar />
       <div className={styles.content}>{children}</div>
     </div>
   </>
