aaa5aa3e1a0e43eaaeffb8edd088c6a531217399
Marcin Filipek <filipekmarcin180@gmail.com>
2022-10-02 16:54:39 +0200

    Added contentful client, added getPosts function

--- components/PostWidget/PostWidget.js
+++ components/PostWidget/PostWidget.js
@@ -1,8 +1,8 @@
 import styles from "./PostWidget.module.scss";
 
-export const PostWidget = () => (
+export const PostWidget = ({ title }) => (
   <div className={styles.container}>
-    <span className={styles.title}>Title</span>
+    <span className={styles.title}>{title}</span>
     <p className={styles.description}>Note</p>
   </div>
 );
