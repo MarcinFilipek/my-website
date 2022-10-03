658b20ab7f31b0da3976602b2ef045fb7424215c
MarcinFilipek <filipekmarcin180@gmail.com>
2022-10-03 21:28:16 +0200

    Added static paths to posts (#5)

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import { PostWidget } from "../components";
 import { Grid } from "@mui/material";
 import { getPosts } from "../api/getPosts";
+import Link from "next/link";
 
 export default function Home({ posts }) {
   return (
     <Grid container spacing={1} sx={{ mt: 20 }}>
       {posts.map(({ sys, fields }) => (
         <Grid item xs={12} md={6} lg={4} key={sys.id}>
-          <PostWidget title={fields.postTitle} />
+          <Link href={`/posts/${sys.id}`} passHref>
+            <a>
+              <PostWidget title={fields.postTitle} />
+            </a>
+          </Link>
         </Grid>
       ))}
     </Grid>
@@ -16,9 +21,6 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const { posts } = await getPosts();
-  if (posts) {
-    console.log(posts);
-  }
   return {
     props: { posts }, // will be passed to the page component as props
   };
