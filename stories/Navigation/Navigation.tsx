254ac275572542382f9bf9f7b76276ab352a0913
Marcin Filipek <filipekmarcin180@gmail.com>
2022-11-22 17:32:02 +0100

    Moves styles to styles file

--- stories/Navigation/Navigation.tsx
+++ stories/Navigation/Navigation.tsx
@@ -1,49 +1,26 @@
 import React from "react";
-import { Typography, useTheme } from "@mui/material";
 import { NavItems } from "./types";
-import Link from "next/link";
+import { Container, StyledUl, StyledLink, StyledTypography } from "./styles";
 
 type Props = {
   navItems: NavItems;
 };
 
-export const Navigation = ({ navItems }: Props) => {
-  const theme = useTheme();
-
-  return (
-    <nav
-      style={{
-        border: `1px solid ${theme.palette.primary.main}66`,
-        background: "#100d21",
-        borderRadius: "0.8rem",
-        display: "inline-block",
-        padding: "20px",
-      }}
-      data-testid="navigation"
-    >
-      <ul
-        style={{
-          display: "flex",
-          listStyle: "none",
-          gap: "50px",
-          padding: "0",
-          margin: "0",
-        }}
-      >
-        {navItems.map((navItem) => (
-          <li key={navItem.id}>
-            <Link href={navItem.href} style={{ textDecoration: "none" }}>
-              <Typography
-                variant="body1"
-                style={{ color: theme.palette.primary.light }}
-                data-testid={`nav-item-${navItem.id}`}
-              >
-                {navItem.text}
-              </Typography>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+export const Navigation = ({ navItems }: Props) => (
+  <Container>
+    <StyledUl>
+      {navItems.map((navItem) => (
+        <li key={navItem.id}>
+          <StyledLink href={navItem.href}>
+            <StyledTypography
+              variant="body1"
+              data-testid={`nav-item-${navItem.id}`}
+            >
+              {navItem.text}
+            </StyledTypography>
+          </StyledLink>
+        </li>
+      ))}
+    </StyledUl>
+  </Container>
+);
---
+++ stories/Navigation/styles.ts
@@ -0,0 +1,27 @@
+import { styled } from "@mui/material/styles";
+import Link from "next/link";
+import { Typography } from "@mui/material";
+
+export const Container = styled("nav")(
+  ({ theme }) => `
+        border: 1px solid ${theme.palette.primary.main}66;
+        background: #100d21;
+        border-radius: 0.8rem;
+        display: inline-block;
+        padding: 20px;`
+);
+
+export const StyledUl = styled("ul")(
+  ({ theme }) => `
+        display: flex;
+          listStyle: none;
+          gap: 50px;
+          padding: 0;
+          margin: 0;`
+);
+
+export const StyledLink = styled(Link)(() => `text-decoration: none;`);
+
+export const StyledTypography = styled(Typography)(
+  ({ theme }) => `color: ${theme.palette.primary.light};`
+);
